e2dfd9c06e98ade191cdcc08f5a9045a7ec0283f
ahmad qorib <ahmadqorib97@gmail.com>
2018-06-06 02:38:34 +0700

    commit pertama

---
+++ app/src/main/java/com/gmail/ahmadqorib97/pertemuan11peta/InfoWindow.java
@@ -0,0 +1,41 @@
+package com.gmail.ahmadqorib97.pertemuan11peta;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+public class InfoWindow implements GoogleMap.InfoWindowAdapter{
+    private Context context;
+
+    public InfoWindow(Context ctx){
+        context = ctx;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        View view = ((Activity)context).getLayoutInflater()
+                .inflate(R.layout.info_window, null);
+
+        TextView ket = view.findViewById(R.id.ket);
+        ImageView img = view.findViewById(R.id.gambar);
+
+        InfoWindowData infoWindowData = (InfoWindowData) marker.getTag();
+
+        int imageId = context.getResources().getIdentifier(infoWindowData.getImage().toLowerCase(),
+                "drawable", context.getPackageName());
+        img.setImageResource(imageId);
+
+        ket.setText(infoWindowData.getKet());
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/gmail/ahmadqorib97/pertemuan11peta/InfoWindowData.java
@@ -0,0 +1,27 @@
+package com.gmail.ahmadqorib97.pertemuan11peta;
+
+/**
+ * Created by aqor on 05/06/18.
+ */
+
+public class InfoWindowData {
+    private String image;
+    private String ket;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getKet() {
+        return ket;
+    }
+
+    public void setKet(String ket) {
+        this.ket = ket;
+    }
+
+}
